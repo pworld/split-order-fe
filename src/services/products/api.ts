cce678457c091f5936dd3f66fc13fc5f473d0a8b
pworld <prameswara888@gmail.com>
2025-02-19 18:58:56 +0700

    Add middleware auth

--- src/services/products/api.ts
+++ src/services/products/api.ts
@@ -2,25 +2,44 @@ import axios from "axios";
 import { Package, Product } from "../../model/types";
 
 const API_URL = import.meta.env.VITE_API_BASE_URL;
+const API_TOKEN = import.meta.env.VITE_API_TOKEN;
+
+// TODO: Improvement: make default configurations for axios to handle headers authorization
 
 // Fetch all products
 export const fetchProducts = async (): Promise<Product[]> => {
-  const response = await axios.get(`${API_URL}/products`);
+  const response = await axios.get(`${API_URL}/products`, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
   return response.data;
 };
 
 // Add new product
 export const addProduct = async (product: Omit<Product, "id">) => {
-  await axios.post(`${API_URL}/products`, {item:product});
+  await axios.post(`${API_URL}/products`, {item:product}, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
 };
 
 // Delete product by ID
 export const deleteProduct = async (id: number) => {
-  await axios.delete(`${API_URL}/products/${id}`);
+  await axios.delete(`${API_URL}/products/${id}`, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
 };
 
 // Place order and get package breakdown
 export const placeOrder = async (selected_items_ids: number[]): Promise<Package[]> => {
-  const response = await axios.post(`${API_URL}/count-charges`, { selected_items_ids });
+  const response = await axios.post(`${API_URL}/count-charges`, { selected_items_ids }, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
   return response.data;
 };
